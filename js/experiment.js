60fd24f2bc75b2bf0c5223db965fdf9790e954dc
j03hanafi <namoapak14@gmail.com>
2020-07-07 20:57:20 +0700

    javascript intro

---
+++ js/experiment.js
@@ -0,0 +1,6 @@
+function greet(firstName, lastName, honorific, greeting) {
+  return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+}
+
+console.log(greet("Brian", "Holt", "Lord", "Salutations"));
+console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
